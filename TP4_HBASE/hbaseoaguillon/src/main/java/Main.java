8bde81460e10d29f4125bb402bd373d360984cad
languiol <aguillon_olivier@yahoo.fr>
2016-11-03 18:49:37 +0100

    Add files via upload

---
+++ TP4_HBASE/hbaseoaguillon/src/main/java/Main.java
@@ -0,0 +1,14 @@
+package hadoop.hbase;
+
+
+
+/**
+ * Created by Olivier on 03/11/2016.
+ */
+public class Main {
+
+    public static void main(String[] agrs) {
+        ReadEvalPrintLoop repl = new ReadEvalPrintLoop();
+        repl.process();
+    }
+}
